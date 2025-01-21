6c20da6e3f53cf5af2664794f98229d3d5e85318
jshprz <joshuaperezmelgazo@gmail.com>
2025-01-22 03:26:26 +0800

    Implement Restful API of Meeting in both of server and Client

--- server/controllers/meeting/meeting.js
+++ server/controllers/meeting/meeting.js
@@ -2,23 +2,153 @@ const MeetingHistory = require('../../model/schema/meeting')
 const mongoose = require('mongoose');
 
 const add = async (req, res) => {
-   
+   try {
+        const { 
+            agenda,
+            attendes,
+            attendesLead,
+            location,
+            related,
+            dateTime,
+            notes,
+            createBy,
+        } = req.body;
+        
+        const meeting = new MeetingHistory({
+            agenda,
+            attendes,
+            attendesLead,
+            location,
+            related,
+            dateTime,
+            notes,
+            createBy,
+        });
+
+        await meeting.save();
+        res.status(201).json({ 
+            message: 'Meeting created successfully', 
+            meeting
+        });
+    } catch (error) {
+        console.error('Failed to create meeting:', err);
+        res.status(400).json({ error: 'Failed to create meeting : ', err });
+   }
 }
 
 const index = async (req, res) => {
-    
+    const query = req.query;
+    query.deleted = false;
+    if (query.createBy) {
+        query.createBy = new mongoose.Types.ObjectId(query.createBy);
+    }
+
+    try {
+        let result = await MeetingHistory.aggregate([
+            { $match: query },
+            {
+                $lookup: {
+                    from: 'User',
+                    localField: 'createBy',
+                    foreignField: '_id',
+                    as: 'users'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'Contacts',
+                    localField: 'attendes',
+                    foreignField: '_id',
+                    as: 'contact'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'Leads',
+                    localField: 'attendesLead',
+                    foreignField: '_id',
+                    as: 'Lead'
+                }
+            },
+            { $unwind: { path: '$users', preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: '$contact', preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: '$Lead', preserveNullAndEmptyArrays: true } },
+            { $match: { 'users.deleted': false } },
+            { $project: { users: 0, contact: 0, Lead: 0 } },
+        ]);
+        res.send(result);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
 }
 
 const view = async (req, res) => {
-    
+    try {
+        let response = await MeetingHistory.findOne({ _id: req.params.id })
+        if (!response) return res.status(404).json({ message: "No Data Found." });
+
+        let result = await MeetingHistory.aggregate([
+            { $match: { _id: response._id } },
+            {
+                $lookup: {
+                    from: 'Contacts',
+                    localField: 'attendes',
+                    foreignField: '_id',
+                    as: 'contact'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'Leads',
+                    localField: 'attendesLead',
+                    foreignField: '_id',
+                    as: 'Lead'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'User',
+                    localField: 'createBy',
+                    foreignField: '_id',
+                    as: 'users'
+                }
+            },
+            { $unwind: { path: '$contact', preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: '$users', preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: '$Lead', preserveNullAndEmptyArrays: true } },
+            { $match: { 'users.deleted': false } },
+            { $project: { contact: 0, users: 0, Lead: 0 } },
+        ]);
+        
+        res.status(200).json(result[0]);
+    } catch (err) {
+        console.log('Error:', err);
+        res.status(400).json({ Error: err });
+    }
 }
 
 const deleteData = async (req, res) => {
-  
+    try {
+        const result = await MeetingHistory.findByIdAndUpdate(req.params.id, { deleted: true });
+        res.status(200).json({ message: "done", result })
+    } catch (err) {
+        res.status(404).json({ message: "error", err })
+    }
 }
 
 const deleteMany = async (req, res) => {
-    
+    try {
+        const result = await MeetingHistory.updateMany({ _id: { $in: req.body } }, { $set: { deleted: true } });
+
+        if (result?.matchedCount > 0 && result?.modifiedCount > 0) {
+            return res.status(200).json({ message: "Meetings Removed successfully", result });
+        } else {
+            return res.status(404).json({ success: false, message: "Failed to remove meetings" })
+        }
+    } catch (err) {
+        return res.status(404).json({ success: false, message: "error", err });
+    }
 }
 
 module.exports = { add, index, view, deleteData, deleteMany }
